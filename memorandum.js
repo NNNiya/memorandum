ead65f220ea3c166028f9d60176d2584d334968c
NNNiya <1539208848@qq.com>
2019-10-23 17:30:02 +0800

    tets

---
+++ memorandum.js
@@ -0,0 +1,15 @@
+
+// var inputContent = document.getElementById("input-content"),
+//     taskSubmit = document.getElementById("task-submit"),
+//     listContent = document.getElementById("list-content");
+
+// function save(){
+//     var title = inputContent.value;
+//     var li = document.createElement("li");
+//     li.classList.add("task-item");
+//     li.innerHTML = '<span class="content">' + title + "</span>" + '<span class="edit"></span>' + '<span class="delete"></span>';
+//     listContent.insertBefore(li, listContent.firstElementChild);
+//     return li;
+// };
+
+// taskSubmit.addEventListener("click",save);
